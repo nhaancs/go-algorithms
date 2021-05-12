42c85dbfbb8391488a30133c4790769de80dff9a
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-12 14:06:11 +0700

    doubly linked list implementation

---
+++ linked_lists/doubly_linked_list.go
@@ -0,0 +1,123 @@
+package main
+
+import "fmt"
+
+type node2 struct {
+	Value interface{}
+	Next *node2
+	Prev *node2
+}
+
+func newNode2(value interface{}) *node2 {
+	return &node2{Value: value}
+}
+
+type linkedList2 struct {
+	Head *node2
+	Tail *node2
+	Length uint
+}
+
+func NewLinkedList2() *linkedList2 {
+	return &linkedList2{}
+}
+
+func (ll *linkedList2) Append(value interface{}) {
+	node := newNode2(value)
+	if ll.Length == 0 {
+		ll.Head = node
+	} else {
+		node.Prev = ll.Tail
+		ll.Tail.Next = node
+	}
+
+	ll.Tail = node
+	ll.Length++
+}
+
+func (ll *linkedList2) Prepend(value interface{}) {
+	node := newNode2(value)
+	if ll.Length == 0 {
+		ll.Tail = node
+	} else {
+		ll.Head.Prev = node
+		node.Next = ll.Head
+	}
+
+	ll.Head = node
+	ll.Length++
+}
+
+func (ll *linkedList2) Insert(index uint, value interface{}) {
+	if (index >= ll.Length) {
+		ll.Append(value)
+		return 
+	}
+
+	if (index == 0) {
+		ll.Prepend(value)
+		return
+	}
+
+	
+	nodeAtIndex := ll.Get(index)
+	beforeNode := nodeAtIndex.Prev
+	insertedNode := newNode2(value)
+
+	beforeNode.Next = insertedNode
+	insertedNode.Prev = beforeNode
+
+	insertedNode.Next = nodeAtIndex
+	nodeAtIndex.Prev = insertedNode
+
+	ll.Length++
+}
+
+func (ll *linkedList2) Get(index uint) *node2 {
+	if (index > ll.Length-1) {
+		return nil
+	}
+
+	runner := ll.Head
+	var i uint
+	for ; i < index; i++ {
+		runner = runner.Next
+	}
+
+	return runner
+}
+
+func (ll *linkedList2) Remove(index uint) {
+	if index > ll.Length-1 {
+		return
+	}
+
+	if index == 0 {
+		ll.Head = ll.Head.Next
+		ll.Length--
+		return
+	}
+	
+	if index == ll.Length-1 {
+		ll.Tail = ll.Tail.Prev
+		ll.Tail.Next = nil
+		ll.Length-- 
+		return
+	}
+	
+	removedNode := ll.Get(index)
+	removedNode.Prev.Next = removedNode.Next
+	removedNode.Next.Prev = removedNode.Next
+	ll.Length--
+}
+
+func (ll *linkedList2) Print() {
+	arr := []interface{}{}
+	
+	for runner := ll.Head; runner != nil; runner = runner.Next {
+		arr = append(arr, *runner)
+	}
+
+	fmt.Println(arr)
+}
+
--- linked_lists/readme.md
+++ linked_lists/readme.md
@@ -1,4 +1,4 @@
-# Linked lists
+# Linked lists & Doubly linked lists
 Fast insert, delete, items in linked list are ordered but not located right next to each other like array items.
 
 ![linked list](./linked-list.jpg)
@@ -10,3 +10,23 @@ Fast insert, delete, items in linked list are ordered but not located right next
 - insert O(n) // most case faster than in an array
 - delete O(n) // most case faster than in an array
 
+## Single vs double linked list
+![single-vs-double-linked-list](./single-vs-double-linked-list.jpg)
+
+Single linked list:
+- Pros
+    - Simple implementation
+    - Require less memory
+    - Delete, insert is a little bit faster
+- Cons
+    - Can not traverse from back to front
+    - A little bit slower at seaching
+
+Double linked list: 
+- Pros
+    - Can traverse from back to front
+    - Can delete the previous node, do searching easier, faster
+- Cons
+    - Complex implementation
+    - Require more memory
+    - Delete, insert is a little bit slower
