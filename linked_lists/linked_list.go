bd876250a62a793dc1c5cb7f48c9671e40ca3c25
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-12 14:50:30 +0700

    reverse single linked list

--- linked_lists/linked_list.go
+++ linked_lists/linked_list.go
@@ -115,3 +115,35 @@ func (ll *linkedList) Print() {
 	fmt.Println(arr)
 }
 
+func (ll *linkedList) Reverse() {
+	if ll.Length <= 1 {
+		return
+	}
+
+	// nodeArr := make([]*node, ll.Length)
+	// i := 0
+	// for runner := ll.Head; runner != nil; runner = runner.Next {
+	// 	nodeArr[i] = runner
+	// 	i++
+	// }
+
+	// for j := len(nodeArr)-1; j > 0; j-- {
+	// 	nodeArr[j].Next = nodeArr[j-1]
+	// }
+	// nodeArr[0].Next = nil
+	// ll.Tail = nodeArr[0]
+	// ll.Head = nodeArr[len(nodeArr)-1]
+
+	first := ll.Head
+	second := first.Next
+	for second != nil {
+		temp := second.Next
+		second.Next = first
+		first = second
+		second = temp
+	}
+	ll.Head.Next = nil
+	ll.Tail = ll.Head
+	ll.Head = first
+}
+
