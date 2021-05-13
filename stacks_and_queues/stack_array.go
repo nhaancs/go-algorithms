6466aaaa6740139a60b2634e02460bc842c97285
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-13 13:44:09 +0700

    stack array implementation

---
+++ stacks_and_queues/stack_array.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+type stackArray struct {
+	data []interface{}
+}
+
+func NewStackArray() *stackArray {
+	return &stackArray{}
+}
+
+func (starr *stackArray) Peek() interface{} {
+	if len(starr.data) == 0 {
+		return nil
+	}
+	return starr.data[len(starr.data)-1]
+}
+
+func (starr *stackArray) Push(value interface{}) {
+	starr.data = append(starr.data, value)
+}
+
+func (starr *stackArray) Pop() interface{} {
+	totalItems := len(starr.data)
+	if totalItems == 0 {
+		return nil
+	}
+
+	lastTop := starr.data[totalItems-1]
+
+	if totalItems == 1 {
+		starr.data = starr.data[:0]
+	} else {
+		starr.data = starr.data[:totalItems-1]
+	}
+	
+	return lastTop
+}
+
+func (starr *stackArray) IsEmpty() bool {
+	return len(starr.data) == 0
+}
+
+func (starr *stackArray) Print() {
+	fmt.Println(starr.data...)
+}
