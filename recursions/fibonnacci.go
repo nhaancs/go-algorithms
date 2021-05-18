2de0c4214a9857380095d9f75fb0f35219c52506
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-18 09:34:26 +0700

    fibonacci

---
+++ recursions/fibonnacci.go
@@ -0,0 +1,15 @@
+package main
+
+// Given an index N, return is the value at the that index of the Fibonacci sequence, where the sequence is:
+// 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144 ...
+// the pattern os the sequence is that each value is the sum of the 2 previous values, that means that for N=5 -> 2+3
+
+func FibonacciNumber(n int) int {
+	if n == 0 {
+		return 0
+	}
+	if n == 1 {
+		return 1
+	}
+	return FibonacciNumber(n-1) + FibonacciNumber(n-2)
+}
