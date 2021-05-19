17b11e65d206c9cb8bd8973a4df828fdaa685fff
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-19 13:28:01 +0700

    recursive bubble sort

--- sorting/bubble_sort.go
+++ sorting/bubble_sort.go
@@ -15,3 +15,18 @@ func BubbleSort(arr []int) {
 		length--
 	}
 }
+
+func BubbleSortRecursive(arr []int) {
+	length := len(arr)
+	if length <= 1 {
+		return
+	}
+
+	for i := 0; i < length - 1; i++ {
+		if arr[i] > arr[i+1] {
+			arr[i], arr[i+1] = arr[i+1], arr[i]
+		}
+	}
+
+	BubbleSortRecursive(arr[:length-1])
+}
--- sorting/main.go
+++ sorting/main.go
@@ -5,7 +5,8 @@ import "fmt"
 func main() {
 	arr := []int{99, 8, 4, 80, 88, 89, 1}
 	fmt.Println(arr)
-	BubbleSort(arr)
+	// BubbleSort(arr)
+	BubbleSortRecursive(arr)
 	fmt.Println(arr)
 }
 
