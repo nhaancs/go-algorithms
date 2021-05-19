ad30e2779f993d85e89f1db7ed32c37b0972a3f3
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-19 13:51:46 +0700

    selection sort

--- sorting/selection_sort.go
+++ sorting/selection_sort.go
@@ -1,6 +1,16 @@
 package main
 
 func SelectionSort(arr []int) {
+	length := len(arr)
+	for i := 0; i < length; i++ {
+		minIndex := i
+		for j := i+1; j < length; j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
 
+		arr[i], arr[minIndex] = arr[minIndex], arr[i]
+	}
 }
 
