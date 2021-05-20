0efca7b5eae1b51ea8ed73a604d43e39c20c25f4
Nhan Nguyen <nhanpublic@gmail.com>
2021-05-20 20:37:16 +0700

    merge sort

--- sorting/main.go
+++ sorting/main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{99, 8, 4, 80, 88, 89, 1}
+	arr := []int{99, 8, 4, 80, 88, 89, 1, 3}
 	fmt.Println(arr)
 	// BubbleSort(arr)
 	// BubbleSortRecursive(arr)
 	// SelectionSort(arr)
 	// SelectionSortRecursive(arr)
 	// InsertionSort(arr)
-	InsertionSortRecursive(arr)
-	fmt.Println(arr)
+	// InsertionSortRecursive(arr)
+	// fmt.Println(arr)
+	fmt.Println(MergeSort(arr))
 }
 
--- sorting/merge_sort.go
+++ sorting/merge_sort.go
@@ -1,5 +1,42 @@
 package main
 
-func MergeSort(arr []int) {
-	
+import (
+	"fmt"
+	"math"
+)
+
+func MergeSort(arr []int) []int {
+	length := len(arr)
+	if length <= 1 {
+		return arr
+	}
+
+	// split array into right and left
+	middle := int(math.Floor(float64(length)/2))
+	left := arr[:middle]
+	right := arr[middle:]
+
+	fmt.Println("middle", middle, "left: ", left, " - ", "right: ", right)
+
+	return merge(MergeSort(left), MergeSort(right))
+}
+
+func merge(left, right []int) []int {
+	result := []int{}
+	leftIndex := 0
+	rightIndex := 0
+
+	for leftIndex < len(left) && rightIndex < len(right) {
+		if left[leftIndex] < right[rightIndex] {
+			result = append(result, left[leftIndex])
+			leftIndex++
+		} else {
+			result = append(result, right[rightIndex])
+			rightIndex++
+		}
+	}
+
+	result = append(result, left[leftIndex:]...)
+	result =  append(result, right[rightIndex:]...)
+	return result
 }
